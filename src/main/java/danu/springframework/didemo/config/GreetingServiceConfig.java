c6217c8aa9c6f71165455a45a477617593ca14e2
Danu Atmaja <danuatmaja@gmail.com>
2021-03-28 18:45:37 +0800

    Spring Framework Configuration

---
+++ src/main/java/danu/springframework/didemo/config/GreetingServiceConfig.java
@@ -0,0 +1,39 @@
+package danu.springframework.didemo.config;
+
+import danu.springframework.didemo.services.GreetingRepository;
+import danu.springframework.didemo.services.GreetingService;
+import danu.springframework.didemo.services.GreetingServiceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    GreetingServiceFactory greetingServiceFactory(GreetingRepository repository){
+        return new GreetingServiceFactory(repository);
+    }
+
+    @Bean
+    @Primary
+    @Profile({"default", "en"})
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    @Bean
+    @Primary
+    @Profile("de")
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("de");
+    }
+
+    @Bean
+    @Primary
+    @Profile("id")
+    GreetingService primaryIndonesianGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("id");
+    }
+}
--- src/main/java/danu/springframework/didemo/services/PrimaryIndonesianGreetingService.java
+++ src/main/java/danu/springframework/didemo/services/PrimaryIndonesianGreetingService.java
@@ -1,12 +1,6 @@
 package danu.springframework.didemo.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
-@Service
-@Profile("id")
-@Primary
 public class PrimaryIndonesianGreetingService implements GreetingService{
 
     private GreetingRepository greetingRepository;
